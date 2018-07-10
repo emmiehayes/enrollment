dbb04d49dff8678803f7cb9f4fc287c825ee078e
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-10 15:03:43 -0600

    WIP test passing for nav links on all pages

---
+++ spec/features/user_can_use_nav_links_from_any_page_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+describe 'a user' do
+  context 'visting show page at student_path' do
+  it 'has links for Create New Student and See All Students' do
+    student_1 = Student.create!(name: 'Emmie Hayes')
+    student_2 = Student.create!(name: 'Michelle Obama')
+
+    visit student_path(student_1)
+    click_link 'Create New Student'
+    expect(current_path).to eq(new_student_path)
+
+    click_link 'See All Students'
+    expect(current_path).to eq(students_path)
+    expect(page).to have_content('Emmie Hayes')
+    expect(page).to have_content('Michelle Obama')
+    end
+  end
+end
+
+describe 'a user' do
+  context 'visting index page at students_path' do
+  it 'can see the name of a single student' do
+    student_1 = Student.create!(name: 'Emmie Hayes')
+    student_2 = Student.create!(name: 'Michelle Obama')
+
+    visit students_path
+    click_link 'Create New Student'
+    expect(current_path).to eq(new_student_path)
+
+    click_link 'See All Students'
+    expect(current_path).to eq(students_path)
+    expect(page).to have_content('Emmie Hayes')
+    expect(page).to have_content('Michelle Obama')
+    end
+  end
+end
+
+xdescribe 'a user' do
+  context 'visting edit page at edit_student_path' do
+    it 'can see the name of a single student' do
+      student = Student.create!(name: 'Emmie Hayes')
+
+      visit student_path(student)
+
+      expect(page).to have_content(student.name)
+    end
+  end
+end
+
+
+describe 'a user' do
+  context 'visting new page at new_student_path' do
+    it 'can see the name of a single student' do
+      student = Student.create!(name: 'Emmie Hayes')
+
+      visit student_path(student)
+
+      expect(page).to have_content(student.name)
+    end
+  end
+end
