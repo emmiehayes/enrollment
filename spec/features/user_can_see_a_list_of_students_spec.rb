9acf2365e6286924fb537cbcd42601a344d164e2
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-10 15:12:40 -0600

    create test for student name link on index, update index view, pass test 99% coverage

--- spec/features/user_can_see_a_list_of_students_spec.rb
+++ spec/features/user_can_see_a_list_of_students_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 describe 'a user' do
   context 'visting students_path' do
-  it 'can see a list of all students' do
-    student_1 = Student.create!(name: 'Emmie Hayes')
-    student_2 = Student.create!(name: 'Michelle Obama')
-    student_3 = Student.create!(name: 'Taylor Reese')
+    it 'can see a list of all students' do
+      student_1 = Student.create!(name: 'Emmie Hayes')
+      student_2 = Student.create!(name: 'Michelle Obama')
+      student_3 = Student.create!(name: 'Taylor Reese')
 
-    visit students_path
+      visit students_path
 
-    expect(page).to have_content('All Students')
-    expect(page).to have_content(student_1.name)
-    expect(page).to have_content(student_2.name)
-    expect(page).to have_content(student_3.name)
+      expect(page).to have_content('All Students')
+      expect(page).to have_content(student_1.name)
+      expect(page).to have_content(student_2.name)
+      expect(page).to have_content(student_3.name)
+    end
+
+    it 'can click a students name and go to the student show page' do
+      student = Student.create!(name: 'Emmie Hayes')
+
+      visit students_path
+      click_link 'Emmie Hayes'
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content(student.name)
     end
   end
 end
