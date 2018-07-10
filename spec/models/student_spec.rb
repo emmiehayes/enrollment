15c22e88a220f5bff3f8eb0865daf54676f3220f
Emmie Hayes <emmiehayes2@gmail.com>
2018-07-10 14:09:53 -0600

    student model spec passing

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'validations' do
   context 'valid entry' do
   it 'has a name' do
-    student = Student.create!(name: 'Emmie Hayes')
+    student = Student.new(name: 'Emmie Hayes')
 
     expect(student).to be_valid
     end
@@ -11,7 +11,7 @@ describe 'validations' do
 
   context 'invalid entry' do
   it 'has a name' do
-    student = Student.create!
+    student = Student.new()
 
     expect(student).to be_invalid
     end
